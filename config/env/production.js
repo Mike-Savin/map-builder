0b301bd82b8446c5d33c422fe45827a548d0afb9
Michael Savin <my.savin@yandex.com>
2016-07-25 15:09:47 +0300

    Change mongodb params

--- config/env/production.js
+++ config/env/production.js
@@ -7,6 +7,8 @@ module.exports = {
       adapter: 'sails-mongo',
       host: 'ds029725.mlab.com',
       port: 29725,
+      user: 'heroku_dcftb3ds',
+      password: 'heroku_dcftb3ds',
       database: 'heroku_dcftb3ds'
     }
   },
