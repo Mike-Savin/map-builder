12373e207f8acfb0e474d008dc9533532fdea91d
Michael Savin <my.savin@yandex.com>
2016-07-25 15:21:06 +0300

    Turn mongo params to environment

--- config/env/production.js
+++ config/env/production.js
@@ -5,11 +5,7 @@ module.exports = {
   connections: {
     production: {
       adapter: 'sails-mongo',
-      host: 'ds029725.mlab.com',
-      port: 29725,
-      user: 'heroku_dcftb3ds',
-      password: 'heroku_dcftb3ds',
-      database: 'heroku_dcftb3ds'
+      url: process.env.MONGOLAB_URI
     }
   },
   port: 80
